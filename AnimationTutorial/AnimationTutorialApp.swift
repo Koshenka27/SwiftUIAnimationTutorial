16c2dd6193c7eccfc00b644852c92c55aa8ebc3f
Logan Koshenka <koshenka27@gmail.com>
2022-03-13 09:37:35 -0400

    Initial Commit

---
+++ AnimationTutorial/AnimationTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTutorialApp.swift
+//  AnimationTutorial
+//
+//  Created by Logan Koshenka on 3/13/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
